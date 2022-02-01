9641198e40bb8c8d68459578a89d8057317c1870
aghassaei <aghassaei@reed.edu>
2022-02-01 15:34:21 -0800

    saving

--- rand-int-mean.c
+++ rand-int-mean.c
@@ -1,2 +1,45 @@
-// c file for rand int mean
-//make repo public
+// NOTES: make repo pubic
+// Program generates a list of random integers and computes the mean (as a real number).
+// Takes length of list and seed as parameters.
+// Prints out the list of integers and the calculated mean. 
+
+int main(){
+
+	// If the length of the list is 0 or less than 0 throw some error
+	
+	// Read in an the length of the int array from the user, store in list_len
+	int list_len;
+	printf("Enter the length of the list of random integers: ");
+	scanf("%d", &list_len);
+
+	// Read in the random seed from the user, store in seed
+	int seed;
+	printf("Enter an integer for the random seed: ")
+	scanf("%d", &seed);
+
+	// Initialize a list of length list_len
+	arr list_len int_list; 
+
+	/*
+	// Populate the array until there is no space left
+	while (list_len > 0){
+		int rand_int;
+		//generate random int
+		//put in array
+		//dealocate rand int
+		list_len--;
+	}
+	
+	*/
+	printf("Integers randomly generated: ")
+	for (/*everything int he array*/){
+		//put in array
+		printf(the thing in array)
+		//add to mean
+	}
+
+	// calculate mean here
+	// print mean
+
+	return 0;
+}
